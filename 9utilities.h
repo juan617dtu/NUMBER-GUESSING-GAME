b083b09a4d4c7e5d2443f72f90913150d298160b
Juan Cuéllar <96099514+juan617dtu@users.noreply.github.com>
2023-01-25 22:47:55 -0500

    Create 9utilities.h

---
+++ 9utilities.h
@@ -0,0 +1,10 @@
+#ifndef UTLITIES_H_    
+#define UTILITIES_H_
+#include <string>
+
+std::string l_trim(const std::string& s);
+std::string t_trim(const std::string& s);
+std::string trim(const std::string& s);
+int random_int(int l);
+
+#endif 
